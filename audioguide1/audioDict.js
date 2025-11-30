7aa36cc3f3247a14454662c8443a690129a5f409
Samuel Braikeh <samuel.braikeh@yahoo.fr>
2025-11-30 12:42:33 +0100

    Deployed 514af41 with MkDocs version: 1.5.3

--- audioguide1/audioDict.js
+++ audioguide1/audioDict.js
@@ -1,81 +1,68 @@
 export const audioDict = {
     0: {
-        "name": "Angélique", 
+        "name": "Angélique officinale", 
         "imgSrc": "../assets/img/dragees.png",
         "imgAlt": "dragées",
-        "audioUrl": "https://blanzat-raconte.github.io/audio/0.mp3"
     },
     1: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     2: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     3: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     4: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     5: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     6: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     7: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     8: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     9: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     10: {
         "name": "", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     11: {
-        "name": "Réglisse et Agastache", 
+        "name": "Réglisse et Agastache Fenouil", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     12: {
-        "name": "", 
+        "name": "Mirabelle et Prunier Reine-Claude", 
         "imgSrc": "",
         "imgAlt": "",
-        "audioUrl": ""
     },
     
 }
